43877ad924005f7af5ea15b6c9b353e65678068c
KhamDD <dangduongkham1714@gmail.com>
2022-08-03 11:52:39 +0700

    khamdd_update twelve astrological page

--- src/app/twelve-astrological-page/twelve-astrological-page.component.ts
+++ src/app/twelve-astrological-page/twelve-astrological-page.component.ts
@@ -11,5 +11,42 @@ export class TwelveAstrologicalPageComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  carousel = document.querySelector('.carousel');
+  currdeg = 0;
+  carouselStyle = {};
+  currIndex = 0;
 
+  handleNextClick() {
+    this.currdeg -= 30;
+    this.setCarouselStyle();
+    this.changeCurrIndex("next");
+  }
+  handlePrevClick() {
+    this.currdeg += 30;
+    this.setCarouselStyle();
+    this.changeCurrIndex("prev");
+  }
+
+  setCarouselStyle() {
+    this.carouselStyle = {
+      "-webkit-transform": "rotateY(" + this.currdeg + "deg)",
+      "-moz-transform": "rotateY(" + this.currdeg + "deg)",
+      "-o-transform": "rotateY(" + this.currdeg + "deg)",
+      "transform": "rotateY(" + this.currdeg + "deg)",
+    }
+  }
+  changeCurrIndex(sign:String) {
+    if(sign === "next") {
+      if(this.currIndex == 11) {
+        this.currIndex = -1;
+      }
+      this.currIndex++;
+    }
+    else {
+      if(this.currIndex == 0) {
+        this.currIndex = 12;
+      }
+      this.currIndex--;
+    }
+  }
 }
