68801d87671ccf79d90c62b3241b848f3bc90ba3
KhamDD <dangduongkham1714@gmail.com>
2022-10-16 23:23:22 +0700

    khamdd_add decode login

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment.prod';
 import { User } from '../_model/User';
 import { BehaviorSubject, Observable, of } from 'rxjs';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,59 +19,34 @@ class Tokens {
   providedIn: 'root',
 })
 export class AuthService {
-  // private currentUserSubject: BehaviorSubject<string>;
-  // public currentUser: Observable<string>;
-  private readonly JWT_TOKEN = 'JWT_TOKEN';
-  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
-  baseUrl = environment.mockApiUrl;
-  private loggedUser: string = '';
+  private baseUrl = environment.mockApiUrl;
+  private helper: any;
+  private decodedToken: any;
+  private token: any;
 
   constructor(private http: HttpClient) {
-    // this.currentUserSubject = new BehaviorSubject<string>(
-    //   localStorage.getItem('currentUser')!
-    // );
-    // this.currentUser = this.currentUserSubject.asObservable();
+    this.token = localStorage.getItem('token')
+    this.helper = new JwtHelperService();
+    if(this.token) {
+      this.decodedToken = this.helper.decodeToken(this.token)
+    }
   }
-
-  // public get currentUserValue() {
-  //   return JSON.parse(this.currentUserSubject.value);
-  // }
-
-  // public setCurrentUserValue(userInfo: any) {
-  //   localStorage.setItem('currentUser', JSON.stringify(userInfo));
-  //   this.currentUserSubject.next(JSON.stringify(userInfo));
-  // }
 
   login(email: string, password: string): Observable<any> {
     return this.http.post(this.baseUrl + '/User/login', {
       email: email, password: password
-    }, {responseType: 'text'}).pipe(
-      map((response:any) => {
-        const token = response;
-        if(token) {
-          localStorage.setItem('token', token);
+    }, { responseType: 'text' }).pipe(
+      map((response: any) => {
+        this.token = response;
+        if (this.token) {
+          localStorage.setItem('token', this.token);
+          this.decodedToken = this.helper.decodeToken(this.token)
         }
       })
     );
   }
 
-  private doLoginUser(email: string, tokens: Tokens) {
-    this.loggedUser = email;
-    this.storeTokens(tokens);
-  }
-
-  private storeTokens(tokens: any) {
-    localStorage.setItem(this.JWT_TOKEN, tokens.jwt);
-    localStorage.setItem(this.REFRESH_TOKEN, tokens.refreshToken);
-  }
-
-  // login(model:any){
-  // return this.http.post(this.baseUrl+'/api/login',model);
-  // }
-
-  logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('currentUser');
-    // this.currentUserSubject.next(null!);
+  getDecodedToken() {
+    return this.decodedToken;
   }
 }
