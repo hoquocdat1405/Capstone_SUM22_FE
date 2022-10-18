d5aff0d7c58af83b370e592be261eeb80fa1e1c7
dat <hoquocdat1405@gmail.com>
2022-10-18 19:29:05 +0700

    dat update login

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -4,10 +4,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment.prod';
 import { User } from '../_model/User';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { JwtHelperService } from "@auth0/angular-jwt";
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
 class Tokens {
@@ -25,39 +25,54 @@ export class AuthService {
   private token: any;
 
   constructor(private http: HttpClient) {
-    this.token = localStorage.getItem('token')
+    this.token = localStorage.getItem('token');
     this.helper = new JwtHelperService();
-    if(this.token) {
-      this.decodedToken = this.helper.decodeToken(this.token)
+    if (this.token) {
+      this.decodedToken = this.helper.decodeToken(this.token);
     }
   }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post(this.baseUrl + 'user/login', {
-      email: email, password: password
-    }, { responseType: 'text' }).pipe(
-      map((response: any) => {
-        this.token = response;
-        if (this.token) {
-          localStorage.setItem('token', this.token);
-          this.decodedToken = this.helper.decodeToken(this.token)
-          localStorage.setItem('role', this.decodedToken.role)
-        }
-      })
-    );
+    return this.http
+      .post(
+        this.baseUrl + 'user/login',
+        {
+          email: email,
+          password: password,
+        },
+        { responseType: 'text' }
+      )
+      .pipe(
+        map((response: any) => {
+          this.token = response;
+          if (this.token) {
+            localStorage.setItem('token', this.token);
+            this.decodedToken = this.helper.decodeToken(this.token);
+            localStorage.setItem('role', this.decodedToken.role);
+          }
+        })
+      );
   }
 
-  register(email:string, name:string, password:string) : Observable<any> {
-    return this.http.post(this.baseUrl + 'user/register', {
-      email: email, name:name, password: password
-    }, {responseType: 'text'}).pipe(
-      map((response:any) => {
-        let res = response;
-        if(res.message === "Success") {
-          console.log("Register successfully");
-        }
-      })
-    )
+  register(email: string, name: string, password: string): Observable<any> {
+    return this.http
+      .post(
+        this.baseUrl + 'user/register',
+        {
+          email: email,
+          name: name,
+          password: password,
+        },
+        { responseType: 'text' }
+      )
+      .pipe(
+        map((response: any) => {
+          let res = response;
+          if (res.message === 'Success') {
+            console.log('Register successfully');
+          }
+        })
+      );
   }
 
   logout() {
