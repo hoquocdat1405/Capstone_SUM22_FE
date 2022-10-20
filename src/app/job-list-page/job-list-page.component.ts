f708ad64100aee4cd013a0416f1730d641b6dc2e
dat <hoquocdat1405@gmail.com>
2022-10-20 13:52:48 +0700

    dat update login

--- src/app/job-list-page/job-list-page.component.ts
+++ src/app/job-list-page/job-list-page.component.ts
@@ -4,17 +4,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-job-list-page',
   templateUrl: './job-list-page.component.html',
-  styleUrls: ['./job-list-page.component.scss']
+  styleUrls: ['./job-list-page.component.scss'],
 })
 export class JobListPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   handleClick() {
     this.router.navigate(['/job-detail']);
   }
 
+  clickFilter(event: any) {
+    var filterBox = document.querySelector('.filter-box');
+    filterBox?.classList.toggle('active');
+    var filterBtn = document.querySelector('.filter-btn');
+
+    filterBtn?.classList.toggle('active');
+  }
 }
--- src/app/mbti-quiz-result-detail-page/mbti-quiz-result-detail-page.component.ts
+++ src/app/mbti-quiz-result-detail-page/mbti-quiz-result-detail-page.component.ts
@@ -4,23 +4,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mbti-quiz-result-detail-page',
   templateUrl: './mbti-quiz-result-detail-page.component.html',
-  styleUrls: ['./mbti-quiz-result-detail-page.component.scss']
+  styleUrls: ['./mbti-quiz-result-detail-page.component.scss'],
 })
 export class MbtiQuizResultDetailPageComponent implements OnInit {
-
   public mbtiType: { name: string; shorthand: string; imgSrc: string };
 
   constructor(private router: Router) {
-    this.mbtiType = {name:'Người che chở',shorthand:'INFJ',imgSrc:'../../assets/svg/mbti-types/mbti-type-infj.svg'}
+    this.mbtiType = {
+      name: 'Người che chở',
+      shorthand: 'INFJ',
+      imgSrc: '../../assets/svg/mbti-types/mbti-type-infj.svg',
+    };
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   suggestClickHandler() {
     this.router.navigate(['/job-list']);
   }
 
-  
+  chooseTab(event: any, index: number) {
+    var tab = document.querySelector(`.tab-item:nth-child(${index})`);
+    var tabActive = document.querySelector(`.tab-item.active`);
+    tabActive?.classList.remove('active');
+    tab?.classList.add('active');
 
+    var infoContent = document.querySelector(
+      `.info-content:nth-child(${index})`
+    );
+    var infoContentAcTive = document.querySelector(`.info-content.active`);
+    infoContentAcTive?.classList.remove('active');
+    infoContent?.classList.add('active');
+
+    // var characterInfo = document.querySelector('.character-info');
+    // characterInfo?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
