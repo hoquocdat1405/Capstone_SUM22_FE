874142bc27156127c3a4280e24189c31d7105dcb
LiemNT <nguyenthanhliem1708@gmail.com>
2022-10-28 13:06:08 +0700

    LiemNT - fix routing from quiz-listing-page to each test attempt page

--- src/app/quiz-listing-page/quiz-listing-page.component.ts
+++ src/app/quiz-listing-page/quiz-listing-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../_services/shared.service';
 import { Test } from '../_model/test.model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TestTypeEnum } from '../shared/constants/app-const'
 @Component({
   selector: 'app-quiz-listing-page',
   templateUrl: './quiz-listing-page.component.html',
@@ -18,11 +19,24 @@ export class QuizListingPageComponent implements OnInit {
       console.log(this.tests.length);
       this.tests.forEach(test => {
       test.introduction = test.introduction.slice(0,150) + "...";
-      
     });
     });
-    
+  }
 
+
+  redirectTest(id:number){
+    console.log('inside');
+    
+    var redirectStr:string="";
+
+    if(id == TestTypeEnum.MBTI_TEST_ID) redirectStr = TestTypeEnum.MBTI_TEST;
+    else if(id == TestTypeEnum.DISC_TEST_ID) redirectStr = TestTypeEnum.DISC_TEST;
+    else if(id == TestTypeEnum.BIGFIVE_TEST_ID) redirectStr = TestTypeEnum.BIGFIVE_TEST;
+    else if(id  == TestTypeEnum.HOLLAND_TEST_ID) redirectStr = TestTypeEnum.HOLLAND_TEST;
+    console.log('id : ' + id);
+    console.log(redirectStr + '-quiz-attempt');
+
+    this.router.navigate([redirectStr + '-quiz-attempt']);
   }
 
 }
