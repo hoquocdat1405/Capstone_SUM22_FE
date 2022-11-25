c15a8d26ab5656bce9b1e2964a6de0fa7ec82569
dat <hoquocdat1405@gmail.com>
2022-11-26 00:09:38 +0700

    gan api landing

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,3 +1,5 @@
+import { UniversityService } from './../_services/university.service';
+import { University } from './../_model/uni';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
@@ -6,12 +8,22 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit, AfterViewInit {
-  constructor() {}
+  uniList: University[] = [];
+  uniListRandom: University[] = [];
+  constructor(private uniSer: UniversityService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getData();
+  }
 
   ngAfterViewInit(): void {
     document.querySelector('edu-news-container')?.lastElementChild?.lastChild
       ?.remove;
+  }
+
+  getData() {
+    this.uniSer.getAllUniversity().subscribe((data) => {
+      this.uniList = data;
+    });
   }
 }
