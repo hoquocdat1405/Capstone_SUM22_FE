30578cd0854b5465a732430deca1183829dd4d84
dat <hoquocdat1405@gmail.com>
2022-11-25 23:01:20 +0700

    update giao dien

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit,AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
-  
+  styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent implements OnInit,AfterViewInit {
+export class LandingPageComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
-    document.querySelector('edu-news-container')?.lastElementChild?.lastChild?.remove
-     
+    document.querySelector('edu-news-container')?.lastElementChild?.lastChild
+      ?.remove;
   }
 }
