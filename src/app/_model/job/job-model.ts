da415ef49e8a3a94f5904de53d653d8c2d77a62b
KhamDD <dangduongkham1714@gmail.com>
2022-12-19 01:52:01 +0700

    khamdd_update holland test page

--- src/app/_model/job/job-model.ts
+++ src/app/_model/job/job-model.ts
@@ -13,3 +13,36 @@ export interface JobMajorModel {
   imageUrl: string;
   majorList: MajorModel[]
 }
+
+export interface MyJobModel {
+  id: number;
+  jobName: string;
+  description: string;
+  imageUrl: string;
+  majors: MyMajor[]
+}
+
+export interface MyMajor {
+  id: string;
+  majorName: string;
+  description: string;
+  majorCode: string;
+}
+
+export interface CharacterJobMajor {
+  id: string;
+  resultName: string;
+  resultSummary: string;
+  resultRelationship: string;
+  resultSuccessRule: string;
+  resultShortName: string;
+  resultPictureUrl: string;
+  resultCareer: string;
+  result1: string;
+  result2: string;
+  result3: string;
+  result4: string;
+  result5: string;
+  result6: string;
+  jobs: MyJobModel[];
+}
--- src/app/holland-quiz-result-detail-page/holland-quiz-result-detail-page.component.ts
+++ src/app/holland-quiz-result-detail-page/holland-quiz-result-detail-page.component.ts
@@ -3,7 +3,7 @@ import { MajorModel } from './../_model/major/major-model';
 import { MajorService } from './../_services/major.service';
 import { SharedService } from './../_services/shared.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { JobModel, JobMajorModel } from './../_model/job/job-model';
+import { JobModel, JobMajorModel, CharacterJobMajor, MyJobModel, MyMajor } from './../_model/job/job-model';
 import { QuizResult } from './../_model/quiz-result';
 import { Title, DomSanitizer } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
@@ -17,39 +17,34 @@ export class HollandQuizResultDetailPageComponent implements OnInit {
   id?: string | null;
   shortName?: string;
   quizResult?: QuizResult;
-  job?: JobModel[];
+  job: JobModel[] = [];
   imageSrc: any;
-  result: { labels: string[]; values: number[] };
+  result?: { labels: string[]; values: number[] };
   showedJobList: JobMajorModel[] = [];
   panelOpenState: boolean = false;
   test?: Test;
   testId: string = '';
   characters: QuizResult[] = [];
 
+  myCharacterList: CharacterJobMajor[] = []
+
+  listJobCareer: (JobModel[])[] = [];
+
+  listValue: string[] = [];
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private sharedServ: SharedService,
-    private sanitizer: DomSanitizer,
     private title: Title,
     private majorService: MajorService
-  ) {
-    this.result = {
-      labels: [
-        'Kỹ thuật',
-        'Nghệ thuật',
-        'Nghiên cứu',
-        'Xã hội',
-        'Quản lí',
-        'Nghiệp vụ',
-      ],
-      values: [35, 1, 32, 29, 16, 24],
-    };
+  ) { 
   }
 
   ngOnInit() {
     this.title.setTitle('Kết quả Holland');
     this.getData();
+
     // this.getCharacter();
   }
 
@@ -61,44 +56,55 @@ export class HollandQuizResultDetailPageComponent implements OnInit {
       .subscribe((result) => {
         if (result !== null) {
           this.quizResult = result;
-          this.getJob();
+          this.listValue.push(this.quizResult?.result1.split('-')[1].split('%')[0]!)
+          this.listValue.push(this.quizResult?.result2.split('-')[1].split('%')[0]!)
+          this.listValue.push(this.quizResult?.result3.split('-')[1].split('%')[0]!)
+          this.listValue.push(this.quizResult?.result4.split('-')[1].split('%')[0]!)
+          this.listValue.push(this.quizResult?.result5.split('-')[1].split('%')[0]!)
+          this.listValue.push(this.quizResult?.result6.split('-')[1].split('%')[0]!)
+          this.result = {
+            labels: [
+              'Kỹ thuật',
+              'Nghiên cứu',
+              'Nghệ thuật',
+              'Xã hội',
+              'Quản lí',
+              'Nghiệp vụ',
+            ],
+            values: [+this.listValue[0], +this.listValue[1], +this.listValue[2], +this.listValue[3], +this.listValue[4], +this.listValue[5]],
+          };
         }
       });
 
-    // console.log(JSON.parse(postAnswer).testId);
     this.testId = JSON.parse(postAnswer).testId;
     this.getCharacter();
   }
 
   getCharacter() {
     this.sharedServ.getTestResult(this.testId, '').subscribe((response) => {
-      this.characters = response;
+      this.myCharacterList = response;
+      this.myCharacterList.forEach(char => {
+        this.sharedServ.getJobCareer(char.id).subscribe({
+          next: (data: MyJobModel[]) => {
+            char.jobs = data;
+            char.jobs.forEach(job => {
+              this.majorService.getMajorCareer(job.id.toString()).subscribe({
+                next: (dataMajor: MyMajor[]) => {
+                  if (dataMajor.length === 0) {
+                    char.jobs.splice(char.jobs.indexOf(char.jobs.find(o => o.id === job.id)!), 1);
+                  }
+                  job.majors = dataMajor;
+                }
+              })
+            })
+          },
+        });
+      })
     });
   }
 
-  getJob() {
-    this.sharedServ.getJobCareer(this.quizResult!.id).subscribe({
-      next: (data) => {
-        this.job = data;
-        this.job?.forEach((job) => {
-          this.majorService.getMajorCareer(job.id.toString()).subscribe({
-            next: (data: MajorModel[]) => {
-              if (data.length === 0) {
-                return;
-              }
-              const jobMajorData: JobMajorModel = {
-                id: job.id,
-                imageUrl: job.imageUrl,
-                description: job.description,
-                jobName: job.jobName,
-                majorList: data,
-              };
-              this.showedJobList.push(jobMajorData);
-            },
-          });
-        });
-      },
-    });
+  retake() {
+    this.router.navigate(['holland-quiz-attempt', { id: this.testId }])
   }
 
   chooseTab(event: any, index: number) {
